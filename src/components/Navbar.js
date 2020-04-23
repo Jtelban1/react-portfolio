231429e61426921ebf109c447f53581dcef01290
John Telban <Jtelban1@gmail.com>
2020-04-23 11:14:45 -0700

    images and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,26 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Navbar = ()=>{
-    return(
+const Navbar = () => {
+    return (
         <header className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <ul className="flex">
                 <li className="mr-6">
-                    <Link to="/" className="text-white hover:underline p-2">JT</Link>
+                    <Link to="/" className="text-white hover:underline p-2">
+                        Home
+                    </Link>
                 </li>
                 <li className="mr-6">
-                    <Link to="/projects" className="text-white hover:underline p-2">Projects</Link>
+                    <Link
+                        to="/projects"
+                        className="text-white hover:underline p-2"
+                    >
+                        Projects
+                    </Link>
                 </li>
             </ul>
         </header>
-    )
-}
+    );
+};
 
 export default Navbar;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Card from "../components/Card";
-import Images from "./../Images";
+import img1 from "../img/Coffee-Club.png";
+import img2 from "../img/Hows-it-growing.png";
+import img3 from "../img/John-Fitness-Tracker.png";
+import img4 from "../img/match-mood.png";
+import img5 from "../img/Note-Taker.png";
+import img6 from "../img/profile-picture.jpg";
 
 class Projects extends React.Component {
     constructor(props) {
@@ -11,42 +16,37 @@ class Projects extends React.Component {
                     gh: "https://github.com/Jtelban1/coffee-club.git",
                     title: "Coffee Club",
                     deployed: "https://fathomless-gorge-36996.herokuapp.com/",
-                    photo: "Coffee-Club Image.png",
+                    photo: img1,
                 },
                 {
-                    gh: "link.me",
-                    title: "Project #1",
-                    deployed: "link2.html",
-                    photo:
-                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                    gh: "https://bfeliz.github.io/match-my-mood/",
+                    title: "Match My Mood",
+                    deployed: "https://bfeliz.github.io/match-my-mood/",
+                    photo: img4,
                 },
                 {
-                    gh: "link.me",
-                    title: "Project #1",
-                    deployed: "link2.html",
-                    photo:
-                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                    gh: "https://github.com/Jtelban1/note-taker",
+                    title: "Note Taker",
+                    deployed: "https://boiling-peak-46769.herokuapp.com/",
+                    photo: img5,
                 },
                 {
-                    gh: "link.me",
-                    title: "Project #1",
-                    deployed: "link2.html",
-                    photo:
-                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                    gh: "https://github.com/Jtelban1/plant-power",
+                    title: "Hows it Growing?",
+                    deployed: "https://hows-itgrowing.herokuapp.com/",
+                    photo: img2,
                 },
                 {
-                    gh: "link.me",
-                    title: "Project #1",
-                    deployed: "link2.html",
-                    photo:
-                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                    gh: "https://github.com/Jtelban1/workout-tracker.git",
+                    title: "Work our Tracker",
+                    deployed: "johns-fitness-tracker.herokuapp.com",
+                    photo: img3,
                 },
                 {
-                    gh: "link.me",
-                    title: "Project #1",
-                    deployed: "link2.html",
-                    photo:
-                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                    gh: "https://github.com/Jtelban1/react-portfolio",
+                    title: "John Telban's Portfolio",
+                    deployed: "uknown",
+                    photo: img6,
                 },
             ],
         };
