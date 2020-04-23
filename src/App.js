cdfa7e6d8710c55d7e837cc1c2eff658385a370b
John Telban <Jtelban1@gmail.com>
2020-04-22 20:48:29 -0700

    images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App ">
-      <Router >
-        <Navbar/>
-        <div className="w-full md:w-2/3 mx-auto pb-16">
-            <Switch >
-                <Route path="/" exact component={Home} />
-                <Route path="/projects"  component={Projects} />
-            </Switch>
+    return (
+        <div className="App ">
+            <Router>
+                <Navbar />
+                <div className="w-full md:w-2/3 mx-auto pb-16">
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/projects" component={Projects} />
+                    </Switch>
+                </div>
+            </Router>
         </div>
-
-      </Router>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,31 +1,72 @@
 import React from "react";
 import Card from "../components/Card";
+import Images from "./../Images";
 
 class Projects extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             cards: [
-                {gh: 'link.me', title: 'Project #1', deployed: 'link2.html', photo: 'https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80'},
-                {gh: 'link.me', title: 'Project #1', deployed: 'link2.html', photo: 'https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80'},
-                {gh: 'link.me', title: 'Project #1', deployed: 'link2.html', photo: 'https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80'},
-                {gh: 'link.me', title: 'Project #1', deployed: 'link2.html', photo: 'https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80'},
-                {gh: 'link.me', title: 'Project #1', deployed: 'link2.html', photo: 'https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80'},
-                {gh: 'link.me', title: 'Project #1', deployed: 'link2.html', photo: 'https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80'}
-            ]
-        }
+                {
+                    gh: "https://github.com/Jtelban1/coffee-club.git",
+                    title: "Coffee Club",
+                    deployed: "https://fathomless-gorge-36996.herokuapp.com/",
+                    photo: "Coffee-Club Image.png",
+                },
+                {
+                    gh: "link.me",
+                    title: "Project #1",
+                    deployed: "link2.html",
+                    photo:
+                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                },
+                {
+                    gh: "link.me",
+                    title: "Project #1",
+                    deployed: "link2.html",
+                    photo:
+                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                },
+                {
+                    gh: "link.me",
+                    title: "Project #1",
+                    deployed: "link2.html",
+                    photo:
+                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                },
+                {
+                    gh: "link.me",
+                    title: "Project #1",
+                    deployed: "link2.html",
+                    photo:
+                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                },
+                {
+                    gh: "link.me",
+                    title: "Project #1",
+                    deployed: "link2.html",
+                    photo:
+                        "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80",
+                },
+            ],
+        };
     }
 
     render() {
-
         return (
             <div className="bg-white border rounded my-16  shadow">
                 <article className="flex flex-wrap">
-                    {this.state.cards.map(card => (
-                        <Card GithubLink={card.gh} DeployedLink={card.deployed} Png={card.photo} Title={card.title}/>))}
+                    {this.state.cards.map((card) => (
+                        <Card
+                            GithubLink={card.gh}
+                            DeployedLink={card.deployed}
+                            Png={card.photo}
+                            Title={card.title}
+                        />
+                    ))}
                 </article>
             </div>
-        )
+        );
     }
 }
 
